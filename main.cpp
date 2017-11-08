305fa74f9e4eb425c21db103af2dd437fcdb8819
Francisco <franciscogthiesen@gmail.com>
2017-11-08 20:19:24 -0200

    Fixing 4 tabs for identation

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ using std::string;
 
 
 // Function for reading CT scans as binary mode file
-vector<unsigned char> readData(const string& fileName){	
+vector<unsigned char> readData(const string& fileName)
+{	
 	std::ifstream file(fileName, std::ios::in | std::ios::binary);
 	vector<unsigned char> contents((std::istreambuf_iterator<char>(file)),
 		std::istreambuf_iterator<char>());
@@ -39,22 +40,24 @@ vector<unsigned char> readData2(const string& fileName)
 // Third attempt at reading CT scans as a binary file
 static std::vector<char> ReadAllBytes(char const* filename)
 {
-    std::ifstream ifs(filename, std::ios::binary | std::ios::ate);
-    std::ifstream::pos_type pos = ifs.tellg();
+	std::ifstream ifs(filename, std::ios::binary | std::ios::ate);
+	std::ifstream::pos_type pos = ifs.tellg();
 
-    std::vector<char>  result(pos);
+	std::vector<char>  result(pos);
 
-    ifs.seekg(0, std::ios::beg);
-    ifs.read(&result[0], pos);
+	ifs.seekg(0, std::ios::beg);
+	ifs.read(&result[0], pos);
 
-    return result;
+	return result;
 }
 
-int main(){
+int main()
+{
 	auto data = readData("head-8bit.raw");
 	auto data2 = readData2("head-8bit.raw");
 	auto data3 = ReadAllBytes("head-8bit.raw");
-	for(auto c : data3){
+	for(auto c : data3)
+	{
 		std::cout << c << " ";
 	}
 	return 0;
